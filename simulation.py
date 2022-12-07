132116b8b3f88b64df6eb8b3f4e2c54db3e16c39
Diane <dianedu@nevada.unr.edu>
2022-12-07 15:28:10 -0800

    Created main simulation file

---
+++ simulation.py
@@ -0,0 +1,9 @@
+# Main file to run for the simulation for CPE 400
+# CPE 400 - Final Project
+# Faulty 
+# Diane Du, Kalista Hong, Abida Mim
+
+
+# main function for simluation
+if __name__ == "__main__":
+    pass
