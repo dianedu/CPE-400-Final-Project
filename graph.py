95ebdc75715b1c83f39eceb6df650b6c7a836115
dian3d <dianedu@nevada.unr.edu>
2022-12-07 22:55:00 -0800

    worked on attributes and some basic class functions

--- graph.py
+++ graph.py
@@ -2,17 +2,28 @@
 import node
 from node import *
 
+#hmmm dictionary data structure may be useful for look up
 class Graph():
     def __init__(self, nodes : set[Node], adjacency : list) -> None:
         self.net_nodes = nodes
         self.adjacency_matrix = [] #list of lists
+        
 
     def __repr__(self) -> str:
         #not sure quite yet how to present
         pass
 
-
+    def breadth_first_search(self) -> None: # hmm maybe better implemented for each node, then nodes have neighbors of node objects?
+        pass
 
 # for ease of testing
 if __name__ == "__main__":
-    pass
+    nodes = set() # might change to a list later
+    nodes.add(Node(1))
+    nodes.add(Node(2))
+    nodes.add(Node(3))
+    nodes.add(Node(4))
+    nodes.add(Node(5))
+
+    adj_list = [[0,1,0,0,0],[1,0,0,1,0],[0,0,0,1,0],[0,0,1,0,1],[0,0,0,0,1]]
+    g = Graph(nodes, adj_list)
--- node.py
+++ node.py
@@ -1,12 +1,43 @@
 # Class to create each router and each router's information 
 
 class Node():
-    def __init__(self) -> None:
-        self.adjacency_list = []
+    def __init__(self, name : int) -> None: 
+        self.label = name # may consider changing to an str type, not entirely sure yet
+        self.adjacency_list = [] # may make more sense to store as a set
+        # dictionary to hold cached routes to each destination router
 
     def __repr__(self) -> str:
-        # to do
-        pass
+        # might need to modify later
+        return f"{self.label}: {self.adjacency_list}"
 
     def set_adjacency(self, adjacent_nodes : list) -> None:
+        self.adjacency_list = adjacent_nodes
+
+    def get_adjacency(self) -> list:
+        return self.adjacency_list
+
+    def set_label(self, name : int) -> None:
+        self.label = name
+    
+    def get_label(self) -> int:
+        return self.label
+
+    def get_cached_route(self, dest : int) -> list[int]:
         pass
+
+    # method to update dictionary of cached routes
+        # recursively get route if successful?
+            # how to handle case when unsuccessful?
+# for ease of testing
+if __name__ == "__main__":
+    n1 = Node(1)
+    print(n1)
+    n2 = Node(2)
+    n3 = Node(3)
+    n4 = Node(4)
+    n5 = Node(5)
+    n1.set_adjacency([n2,n3,n4,n5])
+    print(n1) # oh, how interesting, each nodes have their neighbors as well... I wonder if it would be better to just have their labels?
+    n1_neighbors = [n2,n3,n4,n5]
+    n1.set_adjacency([n.get_label() for n in n1_neighbors]) # list comprehension <3
+    print(n1) # this may be better
