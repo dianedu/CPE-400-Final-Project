da4af126e7818aa2bff22e6423303d24f033d91c
Diane <dianedu@nevada.unr.edu>
2022-12-09 14:55:34 -0800

    implemented RREQ_Packet class to be more accurate with the DSR protocol, need to fix the abstract base class later though

--- packet.py
+++ packet.py
@@ -17,9 +17,6 @@ class Packet(ABC):
 
     def get_src(self) -> int:
         return self.src
-
-    def set_src(self, source : int) -> None:
-        self.src = source
     
     def get_dest(self) -> int:
         return self.dest
@@ -30,37 +27,52 @@ class Packet(ABC):
     def get_pack_type(self) -> str:
         return self.type
 
-    @abstractmethod
-    def get_opt(self) -> None:
-        pass
+    # @abstractmethod
+    # def get_opt(self) -> None:
+    #     pass
 
-    @abstractmethod
-    def set_opt(self, option=None) -> None:
-        pass
+    # @abstractmethod
+    # def set_opt(self, option=None) -> None:
+    #     pass
 
 # Class for RREQ packets
 class RREQ_Packet(Packet):
-    def __init__(self, original_source: int, destination: int) -> None:
-        super().__init__(original_source, destination, "RREQ", [])
+    # def __init__(self, original_source: int, destination: int) -> None:
+    #     super().__init__(original_source, destination, "RREQ", [])
+    id_generator = 0
+    def __init__(self, source : int, num_hops : int, target : int) -> None:
+        self.SRC = source
+        self.hop_limit = num_hops
+        self.id = RREQ_Packet.id_generator
+        self.target_address = target
+        self.addresses = []
 
     def __repr__(self) -> str:
-        # to come back to for implementation
-        # return super().__repr__()
-        pass
+        return f"Packet ID: {self.id}\nOriginal source: {self.SRC}\nTarget Destination: {self.target_address}\nHop Limit: {self.hop_limit}\n"
 
-    def get_opt(self) -> None:
-        if self.opt == None:
-            print("RREQ Packet has nothing in its options field") # might need to comment out later
-            return None
-        else: 
-            return self.opt # this might not be helpful thinking about it now
+    def get_src(self) -> int:
+        return self.SRC
 
-    def set_opt(self, option=None) -> None: # might not need this method (safer without)
-        self.set_opt = option
+    def get_hop_limit(self) -> int:
+        return self.hop_limit
 
-    def add_opt(self, option) -> None:
-        self.opt.append(option) #self.opt should be a list to hold all the paths traversed
-        
+    def get_id(self) -> int:
+        return self.id
+
+    def get_target_address(self) -> int:
+        return self.target_address
+
+    # method for nodes to add own address to path the packet has traversed
+    def add_traversed_address(self, address: int) -> None:
+        self.addresses.append(address)
+
+    def get_traversed_addresses(self) -> list[int]:
+        return self.addresses
+
+    @staticmethod
+    def generate_new_id(self) -> None:
+        RREQ_Packet.id_generator += 1
+
 # TO DO
 # # Class for RREP packets
 # class RREP_Packet(Packet):
@@ -88,4 +100,7 @@ class RREQ_Packet(Packet):
 #     def get_opt(self) -> None:
 #         print("Error occurred in routing, path to destination does not exist") #temporary
 
-
+# for testing purposes
+if __name__ == "__main__":
+    p1 = RREQ_Packet(0, 50, 4)
+    print(p1)
